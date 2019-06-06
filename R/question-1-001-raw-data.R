ecf8730ceb04be52b9763275f45d45cc140480a5
Joe Strachan <joe.strachan@ymail.com>
2019-06-06 17:06:38 +0100

    [pids.wellbeing.weather] Version 0.5.0. The introduction of question_1_001_raw_data.R

--- R/question-1-001-raw-data.R
+++ R/question-1-001-raw-data.R
@@ -4,20 +4,36 @@ library(utils)
 # EXPORTED FUNCTION                                #
 #                                                  #
 ####################################################
-#' stage_000
+#' question_1_001_raw_data
+#' 
+#' Download the raw data for both the well-being and the weather datasets.
+#' 
+#' @param weather_station_names_list An OPTIONAL list of weather station name strings, whose data will be downloaded.
+#'                                   Defaults to WEATHER_STATION_NAMES defined within constants.weather-station-names.R.
+#'                                   
+#' @param wellbeing_destination_name An OPTIONAL string describing the destination file name of the well-being dataset.
+#'                                   Defaults to WELLBEING_DEST_FILE_NAME defined within constants.wellbeing.R.
+#'                                   
+#' @param wellbeing_source_name An OPTIONAL string describing the source file name of the well-being dataset.
+#'                              Defaults to WELLBEING_SOURCE_FILE_NAME defined within constants.wellbeing.R.
+#'  
+#' @return A list containing 'wellbeing' and 'weather' elements, which, in turn, describe the success or failure of each file.                           
+#' @usage pids.wellbeing.weather::question_1_001_raw_data()
 #' @export
-stage_000 <- function(
+question_1_001_raw_data <- function(
   weather_station_names_list = WEATHER_STATION_NAMES,
   wellbeing_destination_name = WELLBEING_DEST_FILE_NAME,
   wellbeing_source_name = WELLBEING_SOURCE_FILE_NAME
 ) {
-  load_raw_data_weather(
-    weather_station_names_list
-  )
-  load_raw_data_wellbeing(
+  results <- list()
+  results$wellbeing <- load_raw_data_wellbeing(
     wellbeing_destination_name,
     wellbeing_source_name
   )
+  results$weather <- load_raw_data_weather(
+    weather_station_names_list
+  )
+  results
 }
 ####################################################
 #                                                  #
@@ -59,6 +75,7 @@ build_url_wellbeing <- function(wellbeing_source_file_name) {
 load_raw_data_weather <- function(
     weather_station_names_list
 ) {
+  results <- character()
   for (weather_station_name in weather_station_names_list) {
     dest <- build_path_weather(
       weather_station_name
@@ -67,14 +84,16 @@ load_raw_data_weather <- function(
       weather_station_name
     )
     if (!download_file(dest, url)) {
-      print(
+      results <- append(
+        results,
         paste0(
           FAIL_MESSAGE_LABEL,
           weather_station_name
         )
       )
     } else {
-      print(
+      results <- append(
+        results,
         paste0(
           SUCCESS_MESSAGE_LABEL,
           weather_station_name
@@ -82,6 +101,7 @@ load_raw_data_weather <- function(
       )
     }
   }
+  results
 }
 
 load_raw_data_wellbeing <- function(
@@ -95,18 +115,15 @@ load_raw_data_wellbeing <- function(
     wellbeing_source_name
   )
   if (!download_file(dest, url)) {
-    print(
-      paste0(
-        FAIL_MESSAGE_LABEL,
-        wellbeing_source_name
-      )
+    result <- paste0(
+      FAIL_MESSAGE_LABEL,
+      wellbeing_source_name
     )
   } else {
-    print(
-      paste0(
-        SUCCESS_MESSAGE_LABEL,
-        wellbeing_source_name
-      )
+    result <- paste0(
+      SUCCESS_MESSAGE_LABEL,
+      wellbeing_source_name
     )
   }
+  result
 }
